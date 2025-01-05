491233943a4a10b81cb65363149130822d78463e
Prince abrahams <ccuser@CCs-MacBook-Air.local>
2025-01-05 11:57:13 +0200

    count code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react'
 
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(15)
 
-  const addValue  =() => {
-    setCounter((prevCounter) => prevCounter + 1)
-    setCounter((prevCounter) => prevCounter + 1)
-    setCounter((prevCounter) => prevCounter + 1)
-    setCounter((prevCounter) => prevCounter + 1)
-  } 
+  const [counter, setCounter] = useState(15)
+  //let counter = 15
+
+  const addValue = () => {
+   setCounter((prevCounter) => prevCounter + 1)
+   setCounter((prevCounter) => prevCounter + 1)
+   setCounter((prevCounter) => prevCounter + 1)
+   setCounter((prevCounter) => prevCounter + 1)
+  }
   const removeValue = () => {
     
     setCounter(counter - 1)
   }
-
+  
   return (
     <>
-      <h1>Hello React this is {count} </h1>
-    <h2>This is a count app: {count}</h2>
-    <button
+      <h1>React course with hitesh {counter}</h1>
+      <h2>Counter value : {counter} </h2>
+      <button
       onClick={addValue}
       >Add value</button> {" "}
       <button
